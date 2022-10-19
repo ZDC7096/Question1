8549219f16c964d9d04299d1c25150d8ab447d9d
ZDC7096 <108491910+ZDC7096@users.noreply.github.com>
2022-10-18 19:49:15 -0700

    correct fixed

--- assets/click.js
+++ assets/click.js
@@ -1,7 +1,7 @@
 
     function change (){
         let yes = document.getElementById('next').style.color = 'white';
-        document.getElementById("btnB").innerHTML = "CORRECT";
+        document.getElementById("btnD").innerHTML = "CORRECT";
         document.getElementById('next').innerHTML = 'CLICK FOR NEXT QUESTION!';
         document.getElementById('next').style.backgroundColor = '#b211ee'
     }
